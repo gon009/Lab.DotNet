3c5e69c5b47e35df279d0f17ad97135f0b08552e
Walter <wcarpiom@ucsm.edu.pe>
2016-08-01 20:10:27 -0500

    A test project was added for ATs PENDING to fix the test

---
+++ SoTourn/SoTour.UI.Tests/AcceptanceTests/ShowNews.feature
@@ -0,0 +1,11 @@
+﻿Feature: ShowNews
+	So That I will be able to follow a conversation about the tournament
+	As a Champion’s fan
+	I want ​to get informed about general news.
+	
+@mytag
+Scenario: Showing the news
+	Given that I enter to the option that shows the news
+	When the view is shown
+	Then I should see a list of news
+
---
+++ SoTourn/SoTour.UI.Tests/AcceptanceTests/ShowNewsSteps.cs
@@ -0,0 +1,54 @@
+﻿using TechTalk.SpecFlow;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+
+namespace SoTour.UI.Tests.AcceptanceTests
+{
+    [Binding, TestFixture]
+    public class ShowNewsSteps
+    {
+        private IWebDriver driver;
+        private string baseURL;
+
+        [SetUp]
+        public void SetUpTest()
+        {
+            driver = new ChromeDriver();
+            baseURL = "www.google.com.pe";
+        }
+
+        [Given(@"that I enter to the option that shows the news"), Test]
+        public void GivenThatIEnterToTheOptionThatShowsTheNews()
+        {
+            driver.Navigate().GoToUrl(baseURL);
+        }
+        
+        [When(@"the view is shown"), Test]
+        public void WhenTheViewIsShown()
+        {
+            driver.Navigate().GoToUrl(baseURL + "/generalnews");
+        }
+        
+        [Then(@"I should see a list of news"), Test]
+        public void ThenIShouldSeeAListOfNews()
+        {
+            Assert.IsTrue(IsElementPresent(By.Id("news_page")));
+        }
+
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+    }
+}
