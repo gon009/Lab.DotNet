4dea8c773e21f96613f3a91ec5cfc89884435af7
gon009 <gegarciam95@gmail.com>
2016-08-04 16:18:43 -0500

    Add ShowPlayer.feature Add ShowPlayerSteps.cs with Tests

---
+++ SoTourn/SoTour.UI.Tests/AcceptanceTests/ShowPlayer.feature
@@ -0,0 +1,11 @@
+﻿Feature: ShowPlayer
+	So That I will be able to follow a conversation about players in a Tournament
+	As a Champion’s fan
+	I want ​to get informed about player's information
+
+@mytag
+Scenario: Add two numbers
+	Given that I enter the name of a player
+	And click to search that player
+	When the grid is shown
+	Then I should see that player with his information
---
+++ SoTourn/SoTour.UI.Tests/AcceptanceTests/ShowPlayerSteps.cs
@@ -0,0 +1,69 @@
+﻿using TechTalk.SpecFlow;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+
+
+namespace SoTour.UI.Tests.AcceptanceTests
+{
+    [Binding,TestFixture]
+    public class ShowPlayerSteps
+    {
+        private IWebDriver driver;
+        private string baseURL;
+
+        [SetUp]
+        public void SetUpTest()
+        {
+            driver = new FirefoxDriver();
+            baseURL = "www.google.com.pe";
+        }
+
+        [Given(@"that I enter the name of a player"), Test]
+        public void GivenThatIEnterTheNameOfAPlayer()
+        {
+            driver.Navigate().GoToUrl(baseURL);
+            driver.FindElement(By.Id("lst-ib")).SendKeys("Jugador");
+        }
+
+        [Given(@"click to search that player"), Test]
+        public void GivenClickToSearchThatPlayer()
+        {
+            driver.FindElement(By.Name("btnG")).Click();
+        }
+
+        [When(@"the grid is shown"), Test]
+        public void WhenTheGridIsShown()
+        {
+            driver.Navigate().GoToUrl(baseURL + "/infoaboutplayers");
+        }
+
+        [Then(@"I should see that player with his information"), Test]
+        public void ThenIShouldSeeThatPlayerWithHisInformation()
+        {
+            Assert.IsTrue(IsElementPresent(By.Id("rcnt")));
+        }
+
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+            driver.Dispose();
+        }
+
+    }
+}
